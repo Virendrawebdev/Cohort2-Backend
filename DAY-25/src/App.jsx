0cc01e7b310c983e8ac3858faa2e12aafc72a849
Virendrawebdev <virendra052001@gmail.com>
2026-03-07 00:24:08 +0530

    Face expression detection

---
+++ DAY-25/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+import FaceExpression from './features/Expression/components/FaceExpression'
+
+function App() {
+  return (
+   <FaceExpression/>
+  )
+}
+
+export default App
