3f484761d31534a1a0d9cef6b2a11cda3c316a17
Virendrawebdev <virendra052001@gmail.com>
2026-03-29 08:21:34 +0530

    feat: integrate Tavily API for internet search functionality - Added @tavily/core dependency to Backend and Frontend package.json files. - Implemented searchInternet function in internet.service.js to utilize Tavily API for fetching search results. - Updated ai.service.js to include a new tool for internet searching using the Tavily API. - Modified generateResponse function to use the new searchInternet tool for up-to-date information. - Enhanced chat functionality in useChat.js to handle chat messages more effectively. - Updated Dashboard component to support new message formatting and rendering with remark-gfm for GitHub Flavored Markdown. - Added CSS styles to hide scrollbar in messages.

--- Perplexity/Frontend/src/app/App.jsx
+++ Perplexity/Frontend/src/app/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const auth= useAuth()
 
   useEffect(()=>{
-    auth.handleGetMe
+    auth.handleGetMe()
   },[])
  
   return (
--- Perplexity/Frontend/src/features/chat/pages/Dashboard.jsx
+++ Perplexity/Frontend/src/features/chat/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import ReactMarkdown from 'react-markdown'
 import { useChat } from '../hook/useChat'
+import remarkGfm from 'remark-gfm'
 
 
 const Dashboard = () => {
@@ -18,7 +19,7 @@ const Dashboard = () => {
   }, [])
 
   const handleSubmitMessage = (event) => {
-    event.preventDefault();
+    event.preventDefault()
 
     const trimmedMessage = chatInput.trim()
     if (!trimmedMessage) {
@@ -29,7 +30,7 @@ const Dashboard = () => {
     setChatInput('')
   }
   const openChat = (chatId) => {
-    chat.handleOpenChat(chatId)
+    chat.handleOpenChat(chatId, chats)
   }
 
   return (
@@ -60,7 +61,7 @@ const Dashboard = () => {
                 key={message.id}
                 className={`max-w-[82%] w-fit rounded-2xl px-4 py-3 text-sm md:text-base ${message.role === 'user'
                     ? 'ml-auto rounded-br-none bg-white/12 text-white'
-                    : 'mr-auto border border-white/25 bg-[#0f1626] text-white/90'
+                    : 'mr-auto border-none text-white/90'
                   }`}
               >
                 {message.role === 'user' ? (
@@ -74,6 +75,7 @@ const Dashboard = () => {
                       code: ({ children }) => <code className='rounded bg-white/10 px-1 py-0.5'>{children}</code>,
                       pre: ({ children }) => <pre className='mb-2 overflow-x-auto rounded-xl bg-black/30 p-3'>{children}</pre>
                     }}
+                    remarkPlugins={[remarkGfm]}
                   >
                     {message.content}
                   </ReactMarkdown>
