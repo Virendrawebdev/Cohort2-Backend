4e307dffb24ce91b5227b9675a481ac5ea75e78d
Virendrawebdev <virendra052001@gmail.com>
2026-04-03 11:40:02 +0530

    Typescrpt setup

---
+++ Typescript/index.js
@@ -0,0 +1,6 @@
+"use strict";
+/**
+ * This is a simple TypeScript file that demonstrates basic variable declaration and arithmetic operations.
+ * String, number, boolean, array, tuple, void, never
+ */
+//npx tsc index.ts --ignoreConfig ;  node index.js
---
+++ Typescript/index.ts
@@ -0,0 +1,14 @@
+/**
+ * This is a simple TypeScript file that demonstrates basic variable declaration and arithmetic operations.
+ * String, number, boolean, array, tuple, void, never
+ */
+
+function greet(name:string):void{
+    console.log("Hello" + name)
+}
+greet("virendra")
+
+const a ="virendra"
+const b = 5
+console.log(a -b)
+
